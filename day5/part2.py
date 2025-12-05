8515b22343ea36f5d41531fc70149a4795042b13
Will Warren <willdavidwarren@gmail.com>
2025-12-05 09:45:37 -0500

    day 5 solved

--- day5/part2.py
+++ day5/part2.py
@@ -3,15 +3,38 @@ with open("input.txt", "r") as f:
 
 allranges = [r.strip() for r in parts[0].split("\n") if r.strip() != ""]
 
-unique_ids = set()
 ranges = []
-freshfoods = []
-rangenum = 1
 
 for ra in allranges:
-    print(f"Processing range {rangenum}/{len(allranges)}...")
     limits = ra.split("-")
-    for i in range(int(limits[0]), int(limits[1]) + 1):
-        unique_ids.add(i)
+    ranges.append((int(limits[0]), int(limits[1])))
 
-print(len(unique_ids))
+# sort by min
+ranges = sorted(ranges, key=lambda range: range[0])
+
+min = ranges[0][0]
+max = ranges[0][1]
+finalranges = []
+
+for i, range in enumerate(ranges):
+    thismin = range[0]
+    thismax = range[1]
+
+    # handle edge case where this range is totally inside the current ranges
+    if thismin >= min and thismax <= max:
+        continue
+
+    if thismin <= max:
+        max = thismax
+    else:
+        finalranges.append((min, max))
+        min = thismin
+        max = thismax
+
+finalranges.append((min, max))
+
+total = 0
+for r in finalranges:
+    total += (r[1] - r[0]) + 1
+
+print(f"Total: {total} (Ranges: {len(finalranges)})")
