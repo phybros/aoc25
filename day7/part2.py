a1d73f8aea6c184dbea2b1f2420b94fa9a17b140
Will Warren <willdavidwarren@gmail.com>
2025-12-07 15:09:10 -0500

    day 7 done

--- day7/part2.py
+++ day7/part2.py
@@ -1,109 +1,59 @@
-grid = []
-checks = {}
-unique_splits = set()
-unique_paths = []
-unique_path_count = 0
-splitters = {}
+class Day7:
+    def __init__(self):
+        self.grid = []
+        self.xstart = -1
+        self.splitters = []
 
-with open("input.txt", "r") as f:
-    lines = f.readlines()
-    for y in range(0, len(lines)):
-        row = []
-        for x in range(0, len(lines[y])):
-            if lines[y][x].strip() != "":
-                row.append(lines[y][x])
+        with open("input.txt", "r") as f:
+            lines = f.readlines()
+            for y in range(0, len(lines)):
+                row = []
+                for x in range(0, len(lines[y])):
+                    if lines[y][x].strip() != "":
+                        row.append(lines[y][x])
 
-                if lines[y][x] == "^":
-                    splitters[(y,x)] = "LR"
+                        if lines[y][x] == "^":
+                            self.splitters.append((y, x))
+                        if lines[y][x] == "S":
+                            self.xstart = x
+                self.grid.append(row)
 
-        grid.append(row)
+    def next_splitter(self, x, fromy):
+        for splitter in self.splitters:
+            if splitter[0] > fromy:
+                if splitter[1] == x:
+                    return splitter
 
-def printgrid():
-    for row in grid:
-        print("".join(row))
+        return None
 
-def emitbeam(y, x, path):
-    global unique_path_count
-    ycheck = 0
-    hit = False
-    while not hit:
-        # reached the end
-        if y+ycheck > len(grid) - 1:
-            # print(f"Reached bottom {path}")
-            # unique_paths.append(path)
-            unique_path_count += 1
-            if unique_path_count % 1000000 == 0:
-                print(f"Paths: {unique_path_count}, Current Path: {path}")
-            break
+    def process(self):
+        beams = {}
+        for s in self.splitters:
+            beams[s] = 0
 
-        hit = grid[y+ycheck][x] == "^"
-        # reached another splitter, send off some beams
-        if hit:
-            emitbeam(y+ycheck, x-1, path + "L")
-            emitbeam(y+ycheck, x+1, path + "R")
-        
-        ycheck += 1
+        # get the first one
+        first = self.next_splitter(self.xstart, 0)
+        beams[first] = 1
 
+        paths = 0
+        for s in self.splitters:
+            inbeams = beams[s]
 
+            nextsplitterl = self.next_splitter(s[1] - 1, s[0])
+            if nextsplitterl is not None:
+                beams[nextsplitterl] += inbeams
+            else:
+                paths += inbeams
 
-for y in range(0, len(grid)):
-    for x in range(0, len(grid[y])):
-        if grid[y][x] == "S":
-            emitbeam(y, x, "")
+            nextsplitterr = self.next_splitter(s[1] + 1, s[0])
+            if nextsplitterr is not None:
+                beams[nextsplitterr] += inbeams
+            else:
+                paths += inbeams
 
-# i = 1
-# for p in unique_paths:
-#     print(f"{i}: {p}")
-#     i+=1
-print(unique_path_count)
-    #     if x + 1 < len(grid[y]):
-    #         if grid[y][x+1] == "^" and "L" in splitters[(y,x+1)]:
-    #             emitbeam(y, x)
-    #             splitters[(y,x+1)] = splitters[(y,x+1)].strip("L")
-
-    #     if x - 1 >= 0:
-    #         if grid[y][x-1] == "^" and "R" in splitters[(y,x-1)]:
-    #             emitbeam(y, x)
-    #             splitters[(y,x-1)] = splitters[(y,x-1)].strip("R")
-        
-    # printgrid()
-
-# print(splitters)
-# print(len(unique_splits))
+        return paths
 
 
-
-# print(totalsplitters)
-
-# for y in range(0, len(grid)):
-#     for x in range(0, len(grid[y])):
-#         if grid[y][x] == "^" or grid[y][x] == "S":
-#             checks[(y,x)] = 0
-#             continue
-
-#         if y - 1 >= 0:
-#             if grid[y-1][x] == "S" or grid[y-1][x] == "|":
-#                 grid[y][x] = "|"
-        
-            
-
-#             if x - 1 >= 0:
-#                 if grid[y][x-1] == "^" or grid[y-1][x-1] == "^":
-#                     grid[y][x] = "|"
-        
-#             if x + 1 < len(grid[y]):
-#                 if grid[y][x+1] == "^" or grid[y-1][x+1] == "^":
-#                     grid[y][x] = "|"
-
-# print(checks)
-
-
-# done = False
-
-# while not done:
-#     for y in range(0, len(grid)):
-#         for x in range(0, len(grid[y])):
-#             if grid[y][x] == "^" and grid[y-1][x] == "|":
-#                 splits += 1
-
-# print(f"\nTotal Beam Splits: {splits}")
+if __name__ == "__main__":
+    day7 = Day7()
+    print(f"Total Paths: {day7.process()}")
